65a4d6444ef63b44bccf4de0a20a1ae100a0cd79
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-03-04 22:19:47 -0500

    climbing stairs

---
+++ climbingStairs.js
@@ -0,0 +1,16 @@
+var climbStairs = function(n) {
+    let counter = 2;
+    let res = 2;
+    let next = 2;
+    let temp;
+    let prev = 1;
+    if(n===1) return prev;
+    if(n===2) return next;
+    while (counter!=n){
+        res+= prev;
+        prev= next;
+        next = res;
+        counter ++;
+    }
+    return res;
+};
