ef6e7238acf2a307f46364c14b7f985c18922739
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-03-05 14:44:59 -0500

    reverse words from string

---
+++ reverseWordsInString.js
@@ -0,0 +1,21 @@
+var removeMultipleSpaces = (s) =>{
+    while(s[s.length-1] === ' '){
+        s = s.slice(0, s.length-1);
+    }
+    for(let i =0; i<s.length; i++){
+        if(s[i] === ' ' && s[i+1] === ' '){
+            s = s.slice(0, i+1).concat(s.slice(i+2));
+            i--;
+        }
+    }
+    return s;
+}
+var reverseWords = function(s) {
+    if(!s.includes(' ')) return s;
+    s = removeMultipleSpaces(s);
+    s = s.split(' ').reverse().join(' ');
+    while(s[s.length-1] === ' '){
+        s = s.slice(0, s.length-1);
+    }
+    return s;
+};
