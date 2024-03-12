f6a8221a2ee730a6d8fe0c6871f73c2feec99f72
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-03-11 21:19:14 -0400

    single number

---
+++ singleNumber.js
@@ -0,0 +1,20 @@
+var singleNumber = function(nums) {
+    let checked = [];
+    while(checked.length<Math.round(nums.length/2)-1){
+    for(let i = 0; i<nums.length; i++){
+        for(let j = i+1; j<nums.length; j++){
+            if(nums[j] === nums[i]){
+                checked.push(nums[i])
+            }
+        }
+    }
+
+        }
+    for(let i = 0; i<nums.length; i++){
+            if(!checked.includes(nums[i])){
+                return nums[i];
+            }
+    }
+    return;
+
+};
