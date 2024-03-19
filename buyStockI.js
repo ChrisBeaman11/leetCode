92785b5432d292a5586359f9eeb1cf2f9c54b403
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-03-18 21:05:24 -0400

    best time to sell stock part 1

---
+++ buyStockI.js
@@ -0,0 +1,15 @@
+var maxProfit = function(prices) {
+    let max = 0;
+    let minPrice = prices[0];
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            if((prices[i] - minPrice)>max)
+            max =  prices[i] - minPrice;
+        }
+    }
+
+    return max;
+};
