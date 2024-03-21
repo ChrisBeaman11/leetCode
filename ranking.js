6359dba77e08002299f7be96130f8317c0290eae
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-03-21 17:19:16 -0400

    relative ranks finally correct

--- ranking.js
+++ ranking.js
@@ -1,21 +1,18 @@
 var findRelativeRanks = function(score) {
     let rankObj = {};
-    let sortedArr = score.toSorted();
+    let sortedArr = score.toSorted((a, b) => a - b);
     rankObj["Gold Medal"] = sortedArr[sortedArr.length-1]
     rankObj["Silver Medal"] = sortedArr[sortedArr.length-2]
     rankObj["Bronze Medal"] = sortedArr[sortedArr.length-3]
     for(let i = sortedArr.length -4; i>=0; i--){
         rankObj[(sortedArr.length-i).toString()] = sortedArr[i];
     }
-    // let valArr = Object.values(rankObj);
-    // let keysArr = Object.keys(rankObj);
-    // console.log(valArr);
-    // console.log(keysArr);
-    // let res = [];
-    // for(let i =0; i<score.length; i++){
-    //     let index = valArr.find((ele) => ele === score[i]);
-    //     res.push(keysArr[index]);
-    // }
-    // return res;
-    // need to find out why this is not working from line 10-20
+    let valArr = Object.values(rankObj);
+    let keysArr = Object.keys(rankObj);
+    let res = [];
+    for(let i =0; i<score.length; i++){
+        let index = valArr.findIndex((ele) => ele === score[i]);
+        res.push(keysArr[index]);
+    }
+    return res;
 };
