5e166792b097b180ef91cc0cac070bcf96df5373
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-03-26 12:35:41 -0400

    index of first occuring string

---
+++ indexOfFirstOccString.js
@@ -0,0 +1,10 @@
+var strStr = function(haystack, needle) {
+    for(let i = 0; i< haystack.length; i++){
+        if(haystack[i] === needle[0]){
+            if(needle === haystack.substring(i, i+needle.length)){
+                return i;
+            }
+        }
+    }
+    return -1;
+};
