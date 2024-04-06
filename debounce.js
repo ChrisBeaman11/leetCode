840a53894ab7fbb559e5515725c5b491c30ddaf5
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-04-05 21:14:11 -0400

    debounce

---
+++ debounce.js
@@ -0,0 +1,14 @@
+var debounce = function(fn, t) {
+    this.called = false;
+    let time;
+    return function(...args) {
+        if(this.called){
+            clearTimeout(time);
+            this.called = false;
+    }
+            time = setTimeout(() => {
+            fn(...args);
+}, t);
+        this.called = true;
+    }
+};
