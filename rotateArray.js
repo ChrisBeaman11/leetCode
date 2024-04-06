9be47baa5787f2e5b8790f2e7af1bd76dbfd5f67
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-04-05 21:13:22 -0400

    rotate array

---
+++ rotateArray.js
@@ -0,0 +1,16 @@
+var rotate = function(nums, k) {
+    newArr = [];
+    if(k>0){
+        while(k>nums.length){
+            k -= nums.length;
+        }
+        newArr = nums.splice(0, nums.length-k)
+        let length = newArr.length;
+        for(let i = 0; i<newArr.length; i++){
+            nums.push(newArr.slice(i, i+1));
+        }
+    }
+    return nums;
+
+
+};
