8f05b394810b6c15ab4d174f7b472a5d52c40877
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-05-10 19:50:50 -0400

    to be or not to be

---
+++ toBe.js
@@ -0,0 +1,18 @@
+var expect = function(val) {
+    return {"toBe": (n) => {
+        if(n===val){
+            return true;
+        } else {
+                throw new Error("Not Equal");
+            }
+        },
+            "notToBe": (n) => {
+                if(n!==val){
+                    return true;
+                }
+                else{
+                    throw new Error("Equal");
+                }
+            }
+    }
+};
