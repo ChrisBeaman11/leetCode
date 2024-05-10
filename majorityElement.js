4dcbc524acfffcf013b9d3da749f1c57ed2933b1
Christopher Beaman <ChrisBeaman97@gmail.com>
2024-05-10 19:49:21 -0400

    majority element

---
+++ majorityElement.js
@@ -0,0 +1,14 @@
+var majorityElement = function(nums) {
+    let numCounter = {};
+    for(let i = 0; i<nums.length; i++){
+        if(numCounter[nums[i]]){
+        numCounter[nums[i]] ++;
+        }
+        else{
+            numCounter[nums[i]] = 1;
+        }
+        if(numCounter[nums[i]]>nums.length/2){
+            return nums[i];
+        }
+    }
+};
